6acc1334734469507e2bd5ca861abe18a84e1a61
Michał Leśniewski <mlesniew@gmail.com>
2019-10-06 22:18:47 +0200

    Add serving of static index file

--- src/ventilatore.cpp
+++ src/ventilatore.cpp
@@ -1,5 +1,6 @@
 #include <WiFiManager.h>
 #include <ESP8266WebServer.h>
+#include <FS.h>
 
 #define HOSTNAME "ventilatore"
 
@@ -8,14 +9,22 @@ ESP8266WebServer server;
 void setup()
 {
     Serial.begin(9600);
+    SPIFFS.begin();
+
     WiFi.hostname(HOSTNAME);
 
     WiFiManager wifiManager;
     wifiManager.autoConnect(HOSTNAME, "secret");
 
-    server.on("/", []{
-            server.send(200, "text/plain", "Hello world!");
+    server.on("/version", []{
+            server.send(200, "text/plain", __DATE__ " " __TIME__);
             });
+
+    server.on("/uptime", []{
+            server.send(200, "text/plain", String(millis() / 1000));
+            });
+
+    server.serveStatic("/", SPIFFS, "/index.html");
 
     server.begin();
 }
