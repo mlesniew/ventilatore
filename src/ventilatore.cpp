20aa74404523212eb134cef2352c95ad95352aee
Michał Leśniewski <mlesniew@gmail.com>
2019-10-06 21:58:27 +0200

    Add hostname macro

--- src/ventilatore.cpp
+++ src/ventilatore.cpp
@@ -1,14 +1,17 @@
 #include <WiFiManager.h>
 #include <ESP8266WebServer.h>
 
+#define HOSTNAME "ventilatore"
+
 ESP8266WebServer server;
 
 void setup()
 {
     Serial.begin(9600);
+    WiFi.hostname(HOSTNAME);
 
     WiFiManager wifiManager;
-    wifiManager.autoConnect("ventilatore", "secret");
+    wifiManager.autoConnect(HOSTNAME, "secret");
 
     server.on("/", []{
             server.send(200, "text/plain", "Hello world!");
