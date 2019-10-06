1bfad6a72b091dddc3a16ad2a964be004d8af3bd
Michał Leśniewski <mlesniew@gmail.com>
2019-10-06 21:50:26 +0200

    Add web server

--- src/ventilatore.cpp
+++ src/ventilatore.cpp
@@ -1,4 +1,7 @@
 #include <WiFiManager.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server;
 
 void setup()
 {
@@ -6,9 +9,15 @@ void setup()
 
     WiFiManager wifiManager;
     wifiManager.autoConnect("ventilatore", "secret");
+
+    server.on("/", []{
+            server.send(200, "text/plain", "Hello world!");
+            });
+
+    server.begin();
 }
 
 void loop()
 {
-
+    server.handleClient();
 }
