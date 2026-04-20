3b6ec3fc182e1d1c872ebff29b99c66a8a073ce4
Michał Leśniewski <mlesniew@gmail.com>
2026-04-20 20:39:07 +0200

    Use PicoHA for Home Assistant integration

--- src/hass.cpp
+++ src/hass.cpp
@@ -1,10 +1,12 @@
+#include "hass.h"
+
 #include <Arduino.h>
+#include <PicoHA.h>
 #include <PicoMQTT.h>
 #include <PicoSyslog.h>
 #include <PicoUtils.h>
 
 #include "fancontrol.h"
-#include "hass.h"
 #include "settings.h"
 
 extern FanControl fan_control;
@@ -13,177 +15,71 @@ extern PicoSyslog::SimpleLogger syslog;
 
 namespace {
 
-const String board_id(ESP.getChipId(), HEX);
-const String topic_prefix = "ventilatore/" + board_id + "/";
-
 PicoMQTT::Client mqtt;
+PicoHA::Device device(mqtt, "Ventilatore", "mlesniew", "Ventilatore");
 
-void report_fan_state(bool running) {
-    syslog.printf("Fan is now %s.\n", running ? "running" : "stopped");
-    mqtt.publish(topic_prefix + "state", running ? "ON" : "OFF", 0, true);
-}
-
-void report_fan_mode(FanControl::Mode mode) {
-    const char * modestr = "auto";
-    if (mode == FanControl::ON) { modestr = "on"; }
-    if (mode == FanControl::OFF) { modestr = "off"; }
-
-    syslog.printf("Mode set to %s.\n", modestr);
-    mqtt.publish(topic_prefix + "mode", modestr, 0, true);
-}
-
-void report_target_humidity(double value) {
-    syslog.printf("Target humidity is now %f.\n", value);
-    mqtt.publish(topic_prefix + "target_humidity", String(value), 0, true);
-}
-
-void report_hysteresis(double value) {
-    syslog.printf("Humidity hysteresis is now %f.\n", value);
-    mqtt.publish(topic_prefix + "humidity_hysteresis", String(value), 0, true);
-}
-
-std::list<PicoUtils::WatchInterface *> watches;
-
-}
+}  // namespace
 
 namespace HomeAssistant {
 
-void autodiscovery() {
-    syslog.println(F("Home Assistant autodiscovery messages..."));
-
-    const String board_unique_id = "ventilatore-" + board_id;
-
-    {
-        const String unique_id = board_unique_id + "-fan";
-        const String topic = "homeassistant/fan/" + unique_id + "/config";
-
-        JsonDocument json;
-        json["unique_id"] = unique_id;
-        json["name"] = "Fan";
-        json["availability_topic"] = topic_prefix + "availability";
-        json["state_topic"] = topic_prefix + "state";
-        json["command_topic"] = topic_prefix + "state/set";
-        json["preset_mode_state_topic"] = topic_prefix + "mode";
-        json["preset_mode_command_topic"] = topic_prefix + "mode/set";
-        json["preset_modes"][0] = "auto";
-        json["preset_modes"][1] = "on";
-        json["preset_modes"][2] = "off";
-
-        auto device = json["device"];
-        device["name"] = "Ventilatore";
-        device["identifiers"][0] = board_unique_id;
-        device["configuration_url"] = "http://" + WiFi.localIP().toString();
-        device["sw_version"] = __DATE__ " " __TIME__;
-
-        auto publish = mqtt.begin_publish(topic, measureJson(json), 0, true);
-        serializeJson(json, publish);
-        publish.send();
-    }
-
-    struct ConfigNumber {
-        const char * unique_id;
-        const char * name;
-        const char * topic;
-        unsigned int min;
-        unsigned int max;
-    };
-
-    static const ConfigNumber config_numbers[] = {
-        {"target-humidity", "Target humidity", "target_humidity", 20, 80},
-        {"humidity-hysteresis", "Humidity hysteresis", "humidity_hysteresis", 5, 20},
-    };
-
-    for (const auto & e : config_numbers) {
-        const auto unique_id = board_unique_id + "-" + e.unique_id;
-        const String topic = "homeassistant/number/" + unique_id + "/config";
-        JsonDocument json;
-        json["unique_id"] = unique_id;
-        json["name"] = e.name;
-        json["availability_topic"] = topic_prefix + "availability";
-        json["state_topic"] = topic_prefix + e.topic;
-        json["command_topic"] = topic_prefix + e.topic + "/set";
-        json["retain"] = true;
-        json["unit_of_measurement"] = "%";
-        json["min"] = e.min;
-        json["max"] = e.max;
-        json["step"] = 1;
-        json["device_class"] = "humidity";
-        json["entity_category"] = "config";
-
-        auto device = json["device"];
-        device["name"] = "Ventilatore";
-        device["identifiers"][0] = board_unique_id;
-        device["configuration_url"] = "http://" + WiFi.localIP().toString();
-        device["sw_version"] = __DATE__ " " __TIME__;
-
-        auto publish = mqtt.begin_publish(topic, measureJson(json), 0, true);
-        serializeJson(json, publish);
-        publish.send();
-    }
+String to_string(FanControl::Mode mode) {
+  switch (mode) {
+    case FanControl::ON:
+      return F("On");
+    case FanControl::OFF:
+      return F("Off");
+    case FanControl::AUTO:
+    default:
+      return F("Auto");
+  }
 }
 
 void init() {
-    mqtt.host = settings.hass.server;
-    mqtt.port = settings.hass.port;
-    mqtt.username = settings.hass.username;
-    mqtt.password = settings.hass.password;
-    mqtt.client_id = "ventilatore-" + board_id;
+  mqtt.host = settings.hass.server;
+  mqtt.port = settings.hass.port;
+  mqtt.username = settings.hass.username;
+  mqtt.password = settings.hass.password;
+  mqtt.client_id = "ventilatore-" + String(ESP.getChipId(), HEX);
 
-    mqtt.will.topic = "ventilatore/" + board_id + "/availability";
-    mqtt.will.payload = "offline";
-    mqtt.will.retain = true;
+  PicoHA::add_diagnostic_entities(device);
 
-    mqtt.connected_callback = [] {
-        syslog.println(F("Home Assistant MQTT connected."));
+  auto& fan = device.addFanWithPresetModes<FanControl::Mode, to_string>(
+      "fan", nullptr,
+      {FanControl::Mode::ON, FanControl::Mode::OFF, FanControl::Mode::AUTO});
 
-        // send autodiscovery messages
-        autodiscovery();
+  fan.getter = [] { return fan_control.is_fan_running(); };
+  fan.setter = [](bool state) {
+    fan_control.mode = state ? FanControl::ON : FanControl::OFF;
+  };
 
-        // notify about availability
-        mqtt.publish(mqtt.will.topic, "online", 0, true);
+  fan.preset_mode_getter = [] { return (FanControl::Mode)fan_control.mode; };
+  fan.preset_mode_setter = [](FanControl::Mode mode) {
+    fan_control.mode = mode;
+  };
 
-        // notify about the current state
-        for (auto & watch : watches) { watch->fire(); }
-    };
+  auto& target_humidity =
+      device.addNumber<double>(F("target_humidity"), F("Target Humidity"));
+  target_humidity.bind(&(settings.fan.humidity));
+  target_humidity.min = 20;
+  target_humidity.max = 80;
+  target_humidity.step = 1;
 
-    watches.push_back(new PicoUtils::Watch<bool>([] { return fan_control.is_fan_running(); }, report_fan_state));
-    watches.push_back(new PicoUtils::Watch<FanControl::Mode>([] { return fan_control.mode; }, report_fan_mode));
-    watches.push_back(new PicoUtils::Watch<double>([] { return settings.fan.humidity; }, report_target_humidity));
-    watches.push_back(new PicoUtils::Watch<double>([] { return settings.fan.hysteresis; }, report_hysteresis));
+  auto& humidity_hysteresis = device.addNumber<double>(
+      F("humidity_hysteresis"), F("Humidity Hysteresis"));
+  humidity_hysteresis.bind(&(settings.fan.hysteresis));
+  humidity_hysteresis.min = 5;
+  humidity_hysteresis.max = 20;
+  humidity_hysteresis.step = 1;
 
-    auto handler = [](String payload) {
-        payload.toLowerCase();
-        if (payload == "on") {
-            fan_control.mode = FanControl::ON;
-        } else if (payload == "off") {
-            fan_control.mode = FanControl::OFF;
-        } else if (payload == "auto") {
-            fan_control.mode = FanControl::AUTO;
-        }
-    };
-
-    mqtt.subscribe("ventilatore/" + board_id + "/state/set", handler);
-    mqtt.subscribe("ventilatore/" + board_id + "/mode/set", handler);
-
-    mqtt.subscribe("ventilatore/" + board_id + "/target_humidity/set", [](String payload) {
-        settings.fan.humidity = payload.toDouble();
-        settings.sanitize();
-    });
-    mqtt.subscribe("ventilatore/" + board_id + "/humidity_hysteresis/set", [](String payload) {
-        settings.fan.hysteresis = payload.toDouble();
-        settings.sanitize();
-    });
-
-    mqtt.begin();
+  mqtt.begin();
+  device.begin();
 }
 
 void loop() {
-    mqtt.loop();
-    for (auto & watch : watches) { watch->tick(); }
+  mqtt.loop();
+  device.loop();
 }
 
-bool connected() {
-    return mqtt.connected();
-}
+bool connected() { return mqtt.connected(); }
 
-}
+}  // namespace HomeAssistant
--- src/settings.cpp
+++ src/settings.cpp
@@ -1,73 +1,73 @@
-#include <LittleFS.h>
-
-#include <PicoUtils.h>
-
 #include "settings.h"
 
-extern Print & logger;
+#include <LittleFS.h>
+#include <PicoUtils.h>
+
+extern Print& logger;
 
 namespace {
 double clamp(const double val, const double lo, const double hi) {
-    return val < lo ? lo : (val > hi ? hi : val);
+  return val < lo ? lo : (val > hi ? hi : val);
 }
-}
+}  // namespace
 
 JsonDocument Settings::get_json() const {
-    JsonDocument json;
-    json["fan"]["humidity"] = fan.humidity;
-    json["fan"]["hysteresis"] = fan.hysteresis;
-    json["fan"]["force_timeout_minutes"] = fan.force_timeout_minutes;
-    json["fan"]["max_auto_on_time"] = fan.max_auto_on_time;
-    json["fan"]["min_auto_on_time"] = fan.min_auto_on_time;
-    json["fan"]["max_auto_off_time"] = fan.max_auto_off_time;
-    json["fan"]["min_auto_off_time"] = fan.min_auto_off_time;
-    json["sensor"] = sensor;
-    json["mqtt"]["server"] = mqtt.server;
-    json["mqtt"]["port"] = mqtt.port;
-    json["mqtt"]["username"] = mqtt.username;
-    json["mqtt"]["password"] = mqtt.password;
-    json["net"]["hostname"] = net.hostname;
-    json["net"]["ota_password"] = net.ota_password;
-    json["net"]["syslog"] = net.syslog;
-    json["hass"]["server"] = hass.server;
-    json["hass"]["port"] = hass.port;
-    json["hass"]["username"] = hass.username;
-    json["hass"]["password"] = hass.password;
-    return json;
+  JsonDocument json;
+  json["fan"]["humidity"] = fan.humidity;
+  json["fan"]["hysteresis"] = fan.hysteresis;
+  json["fan"]["force_timeout_minutes"] = fan.force_timeout_minutes;
+  json["fan"]["max_auto_on_time"] = fan.max_auto_on_time;
+  json["fan"]["min_auto_on_time"] = fan.min_auto_on_time;
+  json["fan"]["max_auto_off_time"] = fan.max_auto_off_time;
+  json["fan"]["min_auto_off_time"] = fan.min_auto_off_time;
+  json["sensor"] = sensor;
+  json["mqtt"]["server"] = mqtt.server;
+  json["mqtt"]["port"] = mqtt.port;
+  json["mqtt"]["username"] = mqtt.username;
+  json["mqtt"]["password"] = mqtt.password;
+  json["net"]["hostname"] = net.hostname;
+  json["net"]["ota_password"] = net.ota_password;
+  json["net"]["syslog"] = net.syslog;
+  json["hass"]["server"] = hass.server;
+  json["hass"]["port"] = hass.port;
+  json["hass"]["username"] = hass.username;
+  json["hass"]["password"] = hass.password;
+  return json;
 }
 
-void Settings::load(const JsonDocument & json) {
-    fan.humidity = json["fan"]["humidity"] | 10;
-    fan.hysteresis = json["fan"]["hysteresis"] | 65;
-    fan.force_timeout_minutes = json["fan"]["force_timeout_minutes"] | 15;
-    fan.min_auto_on_time = json["fan"]["min_auto_on_time"] | 20;
-    fan.max_auto_on_time = json["fan"]["max_auto_on_time"] | 60;
-    fan.min_auto_off_time = json["fan"]["min_auto_off_time"] | 20;
-    fan.max_auto_off_time = json["fan"]["max_auto_off_time"] | 720;
-    sensor = json["sensor"] | "";
-    mqtt.server = json["mqtt"]["server"] | "calor.local";
-    mqtt.port = json["mqtt"]["port"] | 1883;
-    mqtt.username = json["mqtt"]["username"] | "";
-    mqtt.password = json["mqtt"]["password"] | "";
-    net.hostname = json["net"]["hostname"] | "ventilatore";
-    net.ota_password = json["net"]["ota_password"] | "";
-    net.syslog = json["net"]["syslog"] | "";
-    hass.server = json["hass"]["server"] | "";
-    hass.port = json["hass"]["port"] | 1883;
-    hass.username = json["hass"]["username"] | "";
-    hass.password = json["hass"]["password"] | "";
-    sanitize();
+void Settings::load(const JsonDocument& json) {
+  fan.humidity = json["fan"]["humidity"] | 65;
+  fan.hysteresis = json["fan"]["hysteresis"] | 10;
+  fan.force_timeout_minutes = json["fan"]["force_timeout_minutes"] | 15;
+  fan.min_auto_on_time = json["fan"]["min_auto_on_time"] | 20;
+  fan.max_auto_on_time = json["fan"]["max_auto_on_time"] | 60;
+  fan.min_auto_off_time = json["fan"]["min_auto_off_time"] | 20;
+  fan.max_auto_off_time = json["fan"]["max_auto_off_time"] | 720;
+  sensor = json["sensor"] | "";
+  mqtt.server = json["mqtt"]["server"] | "calor.local";
+  mqtt.port = json["mqtt"]["port"] | 1883;
+  mqtt.username = json["mqtt"]["username"] | "";
+  mqtt.password = json["mqtt"]["password"] | "";
+  net.hostname = json["net"]["hostname"] | "ventilatore";
+  net.ota_password = json["net"]["ota_password"] | "";
+  net.syslog = json["net"]["syslog"] | "";
+  hass.server = json["hass"]["server"] | "";
+  hass.port = json["hass"]["port"] | 1883;
+  hass.username = json["hass"]["username"] | "";
+  hass.password = json["hass"]["password"] | "";
+  sanitize();
 }
 
 void Settings::sanitize() {
-    fan.humidity = clamp(fan.humidity, 20, 80);
-    fan.hysteresis = clamp(fan.hysteresis, 5, 20);
-    fan.humidity = clamp(fan.humidity, 10.0 + fan.hysteresis / 2, 90.0 - fan.hysteresis / 2);
+  fan.humidity = clamp(fan.humidity, 20, 80);
+  fan.hysteresis = clamp(fan.hysteresis, 5, 20);
+  fan.humidity =
+      clamp(fan.humidity, 10.0 + fan.hysteresis / 2, 90.0 - fan.hysteresis / 2);
 
-    sensor.trim();
+  sensor.trim();
 }
 
 void Settings::print() {
-    serializeJsonPretty(get_json(), logger);
-    logger.println();
+  serializeJsonPretty(get_json(), logger);
+  logger.println();
 }
